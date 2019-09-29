7f1f32cf8c0d151d1f725a2c624a48e6d57c90a9
Frank <dirsky@126.com>
2019-09-29 18:52:25 +0800

    左侧布局

--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -1,15 +1,93 @@
 <template>
-    <div class="goods">
-      goods
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="(item, index) in goods" :key="index" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+            {{item.name}}<!--{{classMap[item.type]}}-->
+          </span>
+        </li>
+      </ul>
     </div>
+    <div class="foods-wrapper">
+
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        name: 'goods'
+  const ERR_OK = 0
+  export default {
+    name: 'goods',
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: {}
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount',
+        'guarantee', 'invoice', 'special']
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.goods = response.data
+          console.log(this.goods)
+        }
+      })
     }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+      .menu-item
+        display table
+        height 54px
+        width 56px
+        padding 0 12px
+        line-height 14px
+        .icon
+          display inline-block
+          vertical-align top
+          width 12px
+          height 12px
+          margin-right 4px
+          background-size 12px 12px
+          background-repeat no-repeat
+          &.decrease
+            bg-image('images/decrease_1')
+          &.discount
+            bg-image('images/discount_1')
+          &.guarantee
+            bg-image('images/guarantee_1')
+          &.invoice
+            bg-image('images/invoice_1')
+          &.special
+            bg-image('images/special_1')
+        .text
+          display table-cell
+          width 56px
+          vertical-align middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size 12px
+    .foods-wrapper
+      flex 1
 
 </style>
