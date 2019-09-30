6553ab76c10665bd1587adeb77945fca139961db
Frank <dirsky@126.com>
2019-09-30 18:47:24 +0800

    控件联动与购物车

--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -40,7 +40,7 @@
       </ul>
     </div>
     <shopcart :delivery-price="seller.deliveryPrice"
-      :min-price="seller.minPrice"></shopcart>
+      :min-price="seller.minPrice" :select-foods="selectFoods"></shopcart>
   </div>
 </template>
 
@@ -88,6 +88,17 @@
           }
         }
         return 0
+      },
+      selectFoods() {
+        let foods = [];
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if (food.count){
+              foods.push(food)
+            }
+          })
+        })
+        return foods
       }
     },
     methods: {
