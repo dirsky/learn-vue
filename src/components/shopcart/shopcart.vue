30049f5158aadfa0f1dc99d6bdfa3499b5538e18
Frank <dirsky@126.com>
2019-09-30 16:52:40 +0800

    购物车left逻辑

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,19 +3,22 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">
-          ￥元
+        <div class="price" :class="{'highlight':totalPrice>0}">
+          ￥{{totalPrice}}元
         </div>
         <div class="desc">
           另需要配送费￥{{deliveryPrice}}元
         </div>
       </div>
       <div class="content-right">
-
+        <div class="pay">
+          ￥{{minPrice}}元起送
+        </div>
       </div>
     </div>
   </div>
@@ -28,6 +31,37 @@
       deliveryPrice: {
         type: Number,
         default: 0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      },
+      selectFoods: {
+        type: Array,
+        default() {
+          return [
+            // {
+            //   price: 13,
+            //   count: 3
+            // }
+          ]
+        }
+      }
+    },
+    computed: {
+      totalPrice() {
+        let total = 0
+        this.selectFoods.forEach((food) => {
+          total += food.price * food.count
+        })
+        return total
+      },
+      totalCount() {
+        let count = 0
+        this.selectFoods.forEach((food) => {
+          count += food.count
+        })
+        return count
       }
     }
   }
@@ -44,6 +78,7 @@
     .content
       display flex
       background #141d27
+      color rgba(255, 255, 255, 0.4)
       .content-left
         flex 1
         .logo-wrapper
@@ -65,10 +100,28 @@
             text-align center
             // background rgba(0, 0, 0, 0.2)
             background #2b343c
+            &.highlight
+              background rgb(0, 160, 220)
             .icon-shopping_cart
               line-height 44px
               font-size 24px
               color #80858a
+              &.highlight
+                color #fff
+          .num
+            position absolute
+            top 0
+            right 0
+            width 24px
+            height 16px
+            line-height 16px
+            text-align center
+            border-radius 16px
+            font-size 9px
+            font-weight 700
+            color #fff
+            background red
+            box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.4)
         .price
           display inline-block
           vertical-align top
@@ -79,15 +132,22 @@
           border-right 1px solid rgba(255, 255, 255, 0.1)
           font-size 16px
           font-weight 700
-          color rgba(255, 255, 255, 0.4)
+          &.highlight
+            color #fff
         .desc
           display inline-block
           vertical-align top
           margin 12px 0 0 12px
           line-height 24px
-          color rgba(255, 255, 255, 0.4)
           font-size: 10px
       .content-right
         flex 0 0 105px
         width 105px
+        .pay
+          height 48px
+          line-height 48px
+          text-align center
+          font-size 12px
+          font-weight 700
+          background #2b333b
 </style>
