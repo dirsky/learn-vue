8fab53d681ee947cc2edb9aa1ceff7ab2181aab2
Frank <dirsky@126.com>
2019-09-30 13:00:17 +0800

    goods左、右侧布局联动

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cartcontrol">
+    <div class="cart-decrease" v-show="food.count>0"></div>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-increase icon-add_circle" @click="addCart"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addCart() {
+        if (!this.food.count) {
+          this.food.count = 1
+        } else {
+          this.food.count++
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+.cartcontrol
+  font-size 0
+  .cart-decrease, .cart-increase
+    display inline-block
+    padding 6px
+    line-height 24px
+    font-size 24px
+    color rgb(0, 160, 220)
+  .cart-count
+    display inline-block
+
+</style>
--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="(item, index) in goods" :key="index" class="menu-item">
+        <li v-for="(item, index) in goods" :key="index"
+          class="menu-item" :class="{'current':currentIndex===index}"
+          @click="selectFood(index, $event)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}<!--{{classMap[item.type]}}-->
@@ -10,9 +12,9 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="(item, index) in goods" :key="index" class="food-list">
+        <li v-for="(item, index) in goods" :key="index" class="food-list" ref="foodListHook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="(food, index) in item.foods" :key="index" class="food-item border-1px">
@@ -23,12 +25,13 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
                 </div>
               </div>
             </li>
@@ -40,6 +43,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import cartcontrol from '../../components/cartcontrol/cartcontrol'
   const ERR_OK = 0
   export default {
     name: 'goods',
@@ -50,7 +55,9 @@
     },
     data () {
       return {
-        goods: {}
+        goods: {},
+        listHeight: [],
+        scrollY: 0
       }
     },
     created() {
@@ -60,9 +67,58 @@
         response = response.body
         if (response.errno === ERR_OK) {
           this.goods = response.data
-          console.log(this.goods)
+          this.$nextTick(() => {
+            this._initScroll()
+            this._calculateHeight()
+          })
         }
       })
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length - 1; i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i
+          }
+        }
+        return 0
+      }
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        })
+        this.foodScroll = new BScroll(this.$refs.foodsWrapper, {
+            probeType: 3
+          })
+        this.foodScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y))
+        })
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodListHook
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      },
+      selectFood(index, event) {
+        if (!event.constructor) {
+          return
+        }
+        let foodList = this.$refs.foodListHook
+        let el = foodList[index]
+        this.foodScroll.scrollToElement(el, 300)
+      }
+    },
+    components: {
+      cartcontrol
     }
   }
 </script>
@@ -86,6 +142,14 @@
         width 56px
         padding 0 12px
         line-height 14px
+        &.current
+          position relative
+          z-index 10
+          margin-top -1px
+          background #fff
+          font-weight 700
+          .text
+            border-none()
         .icon
           display inline-block
           vertical-align top
@@ -145,6 +209,7 @@
             font-size 10px
             color rgb(147, 153, 159)
           .desc
+            line-height 12px
             margin-bottom 8px
           .extra
             .count
@@ -159,4 +224,9 @@
             .old
               text-decoration line-through
               color rgb(147, 153, 159)
+          .cartcontrol-wrapper
+            position absolute
+            right 0
+            bottom 12px
+
 </style>
